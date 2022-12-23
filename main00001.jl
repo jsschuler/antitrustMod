61bbe2202c09ff4e75620152fb11c2915baa6b5c
jsschuler <jsschuler@gmail.com>
2022-12-23 15:30:22 -0500

    modified:   main00001.jl

---
+++ main00001.jl
@@ -0,0 +1,10 @@
+###########################################################################################################
+#            Antitrust Model Main Code                                                                    #
+#            January 2022                                                                                 #
+#            John S. Schuler                                                                              #
+#                                                                                                         #
+#                                                                                                         #
+###########################################################################################################
+
+# first, this code must be comprehensive, meaning, it can handle the parameter sweep itself. 
+# All data can be saved in Julia file structures. 
