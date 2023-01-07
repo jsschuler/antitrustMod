bec8f7679cdbcb1bf48991363d0845d8cdd807fe
jsschuler <jsschuler@gmail.com>
2023-01-07 17:25:44 -0500

    modified:   objects.jl

--- objects.jl
+++ objects.jl
@@ -11,58 +11,8 @@ struct agent
     blissPoint::Float64
 end
  
-abstract type searchEngine
-end
+# now we need a search engine object
+# the search engines are parameterized by a forgetfullness parameter
+# a perfectly forgetful search engine perturbs agent data to be uniform. 
+# a perfectly unforgetful one fits the data as collected and uses it in search
 
-
-# we need an advertizer object
-
-mutable struct advertiser
-    profit::Int64
-end
-
-# now we need search engines
-
-abstract type searchEngine
-end
-
-abstract type cloneEngine
-end
-# now a macro that creates a search engine struct 
-
-searchEngineList=Array{Symbol}(undef,0)
- 
-macro searchGen(learning::Float64,efficiency::Float64)
-    #searchName="test"
-    searchIndex::String=string(sample(1:1000000,1)[1])
-    searchName=Symbol("S"*searchIndex)
-    funcName=Symbol("S"*searchIndex*"Gen")
-    cloneName=Symbol("C"*searchIndex)
-    #println(funcName)
-    quote
-        # generate the struct
-        mutable struct $searchName <: searchEngine
-            efficiency::Float64
-            learning::Float64
-            profit::Float64
-        end 
-
-        struct $cloneName <: cloneEngine
-            efficiency::Float64
-            learning::Float64
-            profit::Float64
-        end
-        # now create the function that generates the struct 
-        index::Int64=length(searchEngineList)
-        push!(searchEngineList,$searchName($efficiency,$learning,0.0))
-        
-
-
-
-        function clone(searchEngine::$searchName)
-
-            return($cloneName($searchName.efficiency,$searchName.learning))
-        end 
-    end
-
-    end   
