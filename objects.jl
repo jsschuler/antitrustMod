586e533ce1c4ac3034e3d322ffc2603c4eeb1657
jsschuler <jsschuler@gmail.com>
2023-01-08 11:27:42 -0500

    modified:   objects.jl 	modified:   functions.jl 	modified:   main00001.jl 	agentInit.jl

--- objects.jl
+++ objects.jl
@@ -1,17 +1,10 @@
 # we need a union type that is either uniform or Beta.
-probType=Union{Uniform{Float64},Beta{Float64}}
 
-# we need a basic agent object
-struct agent
-    privacy::Float64
-    betaObj::Beta{Float64}
-    gammaObj::Gamma{Float64}
-    expUnif::Float64
-    expSubj::Float64
-    blissPoint::Float64
-    history::Dict{Int64,Array{Int64}}
-end
- 
+#@everywhere begin
+#    probType=Union{Uniform{Float64},Beta{Float64}}
+#end
+
+
 # now we need a search engine object
 # the search engines are parameterized by a particular results function
 # for "Google", it estimates parameters from the agent's search history
