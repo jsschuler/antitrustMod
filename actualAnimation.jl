b093ac72ed7f7bf37cb1bfc71a1dbdf27f7504a1
jsschuler <jsschuler@gmail.com>
2023-02-28 20:57:13 -0500

    modified:   actualAnimation.jl

--- actualAnimation.jl
+++ actualAnimation.jl
@@ -3,6 +3,7 @@ using StatsBase
 # begin with some paramaters 
 # how many agents of each type?
 agtTypCnt=[30,15,50,40]
+labelVec=Array(1:length(agtTypCnt))
 # this yields the number of search engines 
 engineCnt=length(agtTypCnt)
 
@@ -26,21 +27,82 @@ numSplit=function(num::Int64)
     return outPut
 end
 
-nameSplit=function()
+numSplit=function(arr::Array{Int64},idx::Int64,k::Int64)
+    
+    if idx==1
+        retArr=cat(repeat([arr[idx]],k),arr[(idx+1):length(arr)],dims=1)
+    elseif idx==length(arr)
+        retArr=cat(arr[1:(length(arr)-1)],repeat([arr[idx]],k),dims=1)
+    else
+        retArr=cat(arr[1:(idx-1)],repeat([arr[idx]],k),arr[(idx+1):length(arr)],dims=1)
+    end 
+end
+
+nameSplit=function(arr::Array{Int64},label::Int64,k::Int64)
+    idx=0
+    for lb in arr
+        println(lb)
+        println(lb==label)
+        if lb==label
+            idx=lb
+            break
+        end
+    end
+    
+    if idx==1
+        retArr=cat(repeat([arr[idx]],k),arr[(idx+1):length(arr)],dims=1)
+    elseif idx==length(arr)
+        retArr=cat(arr[1:(length(arr)-1)],repeat([arr[idx]],k),dims=1)
+    else
+        retArr=cat(arr[1:(idx-1)],repeat([arr[idx]],k),arr[(idx+1):length(arr)],dims=1)
+    end
+end
+
+
+applyFunc=(f,g) -> x -> g(f(x))
+
+nestFunc=function(f...)
+    holdF=applyFunc(f[1],f[2])
+    for i in 3:length(f)
+        holdF=applyFunc(holdF,f[i])
+    end
+    return holdF
+end
+
+
+splitFunc=function(depth::Int64)
+    global agtTypCnt
+    funcList=[]
+    for i in 2:depth 
+        for j in 1:length(agtTypCnt)
+            push!(funcList,x -> numSplit(x,j,i))
+        end
+    end
+end
+
+# now we need a function that calculates the area required given the vector 
+
+areaFunc=function(arr::Array{Int64})
+    # we need an open area between each rank thus, our width is 2k-1
+    # our height is the max length 
+    area=(2*length(arr)-1)*maximum(arr)
+    return area
+end
+
+# now test functions
+f1=x-> x+1
+f2=  x -> x+2 
+f3= x -> x -3
+f4= x -> x^2
+
+# Step 1: We can store each option as a function
+
+
 
 arrangeFunc=function(step::Int64)
     global agtTypCnt
     rankVec=ordinalrank(agtTypCnt)
-    workingVec=agtTypCnt
-    for t in 1:step
-        newVec=[]
-        rankVec=ordinalrank(workingVec)
-        for j in 1:length(rankVec)
-            if rankVec[j]==1
-                numSplit(workingVec[j])
-            end
-        end
-    end
+    
 
 
 end
